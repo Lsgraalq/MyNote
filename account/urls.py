97d2c4d5c2bf31f067775ca0d04ec94d54c03c86
lsgraalq <ilya.merinec@gmail.com>
2024-02-14 06:00:43 -0800

    add more templates

--- account/urls.py
+++ account/urls.py
@@ -10,5 +10,16 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     
-    path('', dashboard, name='dashboard')
+    # url-адреси для зміни пароля
+    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    # dashboard
+    path('', dashboard, name='dashboard'), 
+
+    # url-адреси для скиданню паролю
+    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
