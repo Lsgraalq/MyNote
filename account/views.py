17b099cafa8bdece73248b3b3e46306e65d0e03a
lsgraalq <ilya.merinec@gmail.com>
2024-02-15 01:15:19 -0800

    add registration page

--- account/views.py
+++ account/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 # Create your views here.
 
@@ -11,7 +11,23 @@ from .forms import LoginForm
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
 
-
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Створити новий об'єкт користувача
+            # але поки не зберігати його
+            new_user = user_form.save(commit=False)
+            # Встановити новий пароль
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Зберегти об'єкт User
+            new_user.save()
+            # Створити профіль користувача
+            Profile.objects.create(user=new_user)
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': user_form})
 
 def user_login(request):
     if request.method == 'POST':
